085378944216cf17b8fb08c659dc113480b43f26
Puneet Srivastva <psrivas@amazon.com>
2018-12-25 23:05:16 -0800

    Saving new author in H2 database

--- src/main/java/com/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -46,10 +46,10 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
         //Rod
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "23454", publisher);
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
+        eric.getBooks().add(noEJB);
+        ddd.getAuthors().add(rod);
 
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
     }
 }
